257f98c1cf2f19290e5c22cffe3708c90310d3aa
Matheus Pintor Fernandes Ferreira <mathfer1000@gmail.com>
2024-11-05 10:09:20 -0300

    Atualização de arquivo principal
    Implementação de mudanças no códido principal a respeito da criação de: método de verificação de integridade de senha; método de criptografia de senha.

--- code/main/main.c
+++ code/main/main.c
@@ -1,12 +1,19 @@
 // Importação de bibliotecas
 #include <stdio.h>
+#include <string.h>
+
+// Declaração de constantes
+#define QUANTIDADE_LINHAS_PULAR 3
+#define CODIGO_CARACTERE_SUBSTITUTO 42
 
 // Declaração de procedimentos
 void menuInicial();
 void pularLinhas();
+void criptografaSenha(char* senhaResgatada);
 
 // Declaração de funções
 int resgataOpcao();
+int verificaIntegridadeSenha(char* senhaResgatada);
 
 // Execução do programa
 int main() {
@@ -21,7 +28,7 @@ int main() {
         menuInicial();
         opcaoEscolhida = resgataOpcao();
 
-        // Recolhendo a opção do usuário
+        // Direcionando o usuário
         if (opcaoEscolhida != 0) {
 
             // Redirecionando o usuário
@@ -41,11 +48,11 @@ int main() {
                 default:
                     printf("Encerrando o programa");
                     continuarPrograma = 0;
-            } pularLinhas();
+            } pularLinhas(QUANTIDADE_LINHAS_PULAR);
             
         } else {
             printf("Opcao invalida \n");
-        } pularLinhas();
+        } pularLinhas(QUANTIDADE_LINHAS_PULAR);
 
     }
     
@@ -56,22 +63,107 @@ int main() {
 
 // Inicialização de procedimentos
 void menuInicial() {
+
+    // Mostrando as opções disponíveis
     printf("===== Menu Inicial ===== \n\n1. Cadastro de usuario\n2. Alteracao de usuario\n3. Exclusao de usuario\n4. Listagem de usuarios\n5. Sair\n\n");
+
 }
 
-void pularLinhas() {
-    for (int i = 0; i < 3; i++) {
+void pularLinhas(int quantidadeLinhas) {
+
+    // Pulando uma quantidade determinada de linhas
+    for (int i = 0; i < quantidadeLinhas; i++) {
         printf("\n");
     }
+
+}
+
+void criptografaSenha(char* senhaResgatada) {
+
+    // Declaração de variáveis
+	int tamanhoSenha, somador, codigoNovoCaractere; 
+	somador = tamanhoSenha = strlen(senhaResgatada);
+
+    // Alterando todos os caracteres da senha
+	for (int i = 0; i < tamanhoSenha; i++) {
+
+        // Atribuições
+		codigoNovoCaractere = (int) senhaResgatada[i] + somador;
+
+        // Verificando se o caractere está fora da tabela ASCII
+		if (codigoNovoCaractere > 126) {
+			codigoNovoCaractere = CODIGO_CARACTERE_SUBSTITUTO;
+		} 
+        
+        // Aplicando a mudança
+        senhaResgatada[i] = (char) codigoNovoCaractere;	
+		somador--;
+
+	}	
+
 }
 
 // Inicialização de funções
 int resgataOpcao() {
+
+    // Declaração de variáveis
     int opcao; 
+
+    // Entradas e saídas
     printf("Informe a opcao desejada: ");
     scanf("%d", &opcao);
+
+    // Realizando a verificação
     if (opcao < 1 || opcao > 5) {
         return 0;
     }
     return opcao;
+
+}
+
+int verificaIntegridadeSenha(char* senhaResgatada) {
+
+    // Declaração de variáveis
+    int tamanhoSenha = strlen(senhaResgatada), tamanhoValido, senhaValida;
+
+    // Atribuições
+    tamanhoValido = tamanhoSenha >= 6;
+
+    // Verificando se a senha possui um tamanho válido
+    if (tamanhoValido) {
+
+        // Declaração de variáveis
+        int quantidadeLetrasMaiusculas, quantidadeLetrasMinusculas, quantidadeNumeros, 
+        letraMaiuscula, letraMinuscula, numero, codigoCaractere; 
+
+        // Atribuições
+        quantidadeLetrasMaiusculas = quantidadeLetrasMinusculas = quantidadeNumeros = 0;
+
+        // Verificando se a senha atende aos requisitos
+        for (int i = 0; i < tamanhoSenha; i++) {
+
+            // Atribuições
+            codigoCaractere = (int) senhaResgatada[i];
+            letraMaiuscula = codigoCaractere >= 65 && codigoCaractere <= 90;
+            letraMinuscula = codigoCaractere >= 97 && codigoCaractere <= 122;
+            numero = codigoCaractere >= 48 && codigoCaractere <= 57;
+
+            // Realizando as verificações
+            if (letraMaiuscula) {
+                quantidadeLetrasMaiusculas++;
+            } else if (letraMinuscula) {
+                quantidadeLetrasMinusculas++;
+            } else if (numero) {
+                quantidadeNumeros++;
+            }
+
+        } 
+        
+        // Atribuições
+        senhaValida = quantidadeLetrasMaiusculas >= 1 && quantidadeLetrasMinusculas >= 1 && quantidadeNumeros >= 1;
+
+    } if (senhaValida) {
+        return 1;
+    } return 0;
+
 }
